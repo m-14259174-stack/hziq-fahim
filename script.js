1a9411293f8cd9e8f03975ee3e5a370630009973
m-14259174-stack <m-14259174@moe-dl.edu.my>
2026-07-27 14:21:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
+function getLocation(){
 
+    const status = document.getElementById("status");
+    const result = document.getElementById("result");
+
+    if(!navigator.geolocation){
+        status.innerHTML = "Geolocation tidak disokong.";
+        return;
+    }
+
+    status.innerHTML = "Sedang mendapatkan lokasi...";
+
+    navigator.geolocation.getCurrentPosition(success,error);
+
+    function success(position){
+
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        status.innerHTML = "Lokasi berjaya diperoleh.";
+
+        result.innerHTML = `
+            <h3>Koordinat Anda</h3>
+            <p>Latitude : ${latitude}</p>
+            <p>Longitude : ${longitude}</p>
+
+            <a href="https://www.google.com/maps?q=${latitude},${longitude}" target="_blank">
+                Buka di Google Maps
+            </a>
+        `;
+    }
+
+    function error(){
+        status.innerHTML = "Gagal mendapatkan lokasi.";
+    }
+
+}
